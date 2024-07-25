5c176d56e14bc70ac33e98699da79103a48bd433
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-07-25 16:11:21 +0200

    Created arrays for password generation and merged all in only one

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
 
+$Password_lenght = $_GET["number"];
+/* var_dump($Password_lenght); */
+
+function generatePassword($Password_lenght){
+/* ARRAY PER LA GENERAZIONE DELLA PASSWORD */
+    $lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    $uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    $numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    $symbols = ['!', '?', '&', '%', '$', '<', '>', '^', '+', '-', '*', '/', '(', ')', '[', ']', '{', '}', '@', '#', '_', '='];
+
+    $ArrayWithAllCharacter = array_merge($lowercase, $uppercase, $numbers, $symbols);
+
+
+}
+
 
 
 include __DIR__ . '/partials/head.php';
