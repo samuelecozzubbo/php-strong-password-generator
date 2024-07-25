ab8d4a2a2b3e9941535cfcc86a8db91f39c5a23c
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-07-25 15:53:35 +0200

    Build: created static page layout and add style

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+
+
+include __DIR__ . '/partials/head.php';
+?>
+
+<body>
+    <div class="wrapper">
+        <header class="container">
+            <h1>STRONG PASSWORD GENERATOR</h1>
+            <h2>Genera una password sicura</h2>
+            <div class="password-display">
+                <div>Generare una password compresa fra 8 e 32</div>
+            </div>
+        </header>
+        <main class="container app">
+            <form action="index.php" method="get">
+                <div class="password-lenght flex">
+                    <div>Lunghezza password:</div>
+                    <input type="number" name='number' min="8" max="32">
+                </div>
+                <div class="buttons flex">
+                    <button type="submit">Invia</button>
+                    <button type="reset">Annulla</button>
+                </div>
+            </form>
+        </main>
+    </div>
+</body>
+</html>
